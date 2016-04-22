1aeeed196dde02730db9f6b6ff477838c644a8a3
Melancias <alexintosh@gmail.com>
2016-04-22 13:25:44 +0100

    skel cenas

--- server.py
+++ server.py
@@ -1,27 +1,13 @@
 from flask import Flask
 from flask import request
 from flask import json
-import sqlite3
-
-def createTables():
-    db.executescript(open("./database/tables.sql").read())
-    conndb.commit()
-
-conndb=sqlite3.connect("./database/aitd.db")
-db=conndb.cursor()
-#createTables()
-
-def insertAluno(nome,nacionalidade,idade):
-    db.execute("INSERT INTO alunos (nome, nacionalidade, idade) VALUES (?,?,?)",(nome,nacionalidade,idade))
-    conndb.commit()
+import skel
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
-
 
 @app.route('/messages', methods=['POST'])
 def api_message():
@@ -30,7 +16,7 @@ def api_message():
         return "Text Message: " + request.data
 
     elif request.headers['Content-Type'] == 'application/json':
-        return "JSON Message: " + json.dumps(request.json)
+        return skel.handler(request.json)
 
     elif request.headers['Content-Type'] == 'application/octet-stream':
         f = open('./binary', 'wb')
---
+++ skel.py
@@ -0,0 +1,24 @@
+import sqlite3
+from flask import json
+
+def createTables():
+    db.executescript(open("./database/tables.sql").read())
+    conndb.commit()
+
+conndb=sqlite3.connect("./database/aitd.db")
+db=conndb.cursor()
+#createTables()
+
+def insertAluno(nome,nacionalidade,idade):
+    db.execute("INSERT INTO alunos (nome, nacionalidade, idade) VALUES (?,?,?)",(nome,nacionalidade,idade))
+    conndb.commit()
+
+def handler(test):
+    request=json.loads(test)
+    response=''
+    if request['cmd']=='ADD':
+        insertAluno(request['nome'],request['nacionalidade'],request['idade'])
+        response=json.dumps('OK BABY')
+    else:
+        response=json.dumps('NOT OK BABT')
+    return response
